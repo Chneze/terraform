81123db8c23c60d5888c10e5563bdc375ed35b26
Chneze <121574861+Chneze@users.noreply.github.com>
2025-02-01 23:53:08 +0200

    Update main.tf

--- terraform-state-lock/main.tf
+++ terraform-state-lock/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-   region     = "eu-east-1"
+   region     = "us-east-1"
    access_key = var.access_key
    secret_key = var.secret_key
 }
 
 resource "aws_instance" "ec2_web" {
-    ami = "ami-0767046d1677be5a0"
+    ami = "ami-0c614dee691cbbf37"
     instance_type = "t2.micro"
     tags = {
       Name = "EC2 Instance with remote state"
@@ -16,7 +16,7 @@ terraform {
     backend "s3" {
         bucket = "henokk-terraform-s3-bucket"
         key    = "henok/terraform/remote/s3/terraform.tfstate"
-        region     = "eu-east-1"
+        region     = "us-east-1"
         dynamodb_table = "dynamodb-state-locking"
     }
 }
